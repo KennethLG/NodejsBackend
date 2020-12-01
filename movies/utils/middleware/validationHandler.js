05c78db50ab586ed1992872265cea69cff970dc8
KennethLG <orsonleiziaga@gmail.com>
2020-11-30 21:02:24 -0500

    Joi fixed

--- movies/utils/middleware/validationHandler.js
+++ movies/utils/middleware/validationHandler.js
@@ -1,14 +1,17 @@
 const boom = require("@hapi/boom");
 const joi = require("joi");
+const {movieIdSchema} = require("../schemas/movies");
 
 function validate(data, schema) {
+	if (data.movieId) {
+		return joi.assert(data.movieId, movieIdSchema);
+	}
 	return joi.assert(data, schema);
 }
 
 function validationHandler(schema, check = "body") {
 	return function(req, res, next) {
 		const error = validate(req[check], schema);
-
 		error ? next(boom.badRequest(error)): next();
 	};
 }
